6f6a15ae711cea919d9bafdfb80ee95a5b5cc485
Manish <manishdwivedi45165@gmail.com>
2023-06-28 19:23:29 -0700

    maths basics

---
+++ maths.js
@@ -0,0 +1,15 @@
+20.95+7.99*2
+
+36.93*0.1
+
+Math.round(2.2)
+2
+Math.round(2.8)
+3
+// floating numbers gives inaccuracy in javascript to remove it we always change dollars to cents
+((2095+799)*0.1)/100
+2.894
+M((2095+799)*0.1)/100
+
+Math.round(((2095+799)*0.1))/100
+2.89
