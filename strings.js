795cb691b2c82e97729568682f0334a8087e0ef7
Manish <manishdwivedi45165@gmail.com>
2023-06-28 19:47:59 -0700

    strings basics

---
+++ strings.js
@@ -0,0 +1,26 @@
+'some'
+'some'
+'some'+'text'
+// string concatination
+'some'+'text'+' more'
+
+typeof 2
+
+typeof 'hello'
+
+
+'hello'+3
+
+'$' +(2095+799)/100
+'$28.94'
+'Items (' +(1+1)+'): $' +(2095+799)/100
+'Items (2): $28.94'
+alert('Items (2): $28.94');
+
+"I'm learning Javascript"
+
+alert('some\ntext');//newline charachter
+
+
+// backticks uses in strings
+`Items (${1+1}): $${(2095+799)/100}`
