a8e5651aaa768146bb33ea270bad74f9f443e274
Manish <manishdwivedi45165@gmail.com>
2023-07-21 09:48:18 +0530

    todo list project

---
+++ 11-todo-list.js
@@ -0,0 +1,46 @@
+const todoList = [{name: 'wash dishes',dueDate:'22-12-2022'},
+{name: 'make dinner', dueDate: '11-4-2022'}];
+renderTodoList();
+function renderTodoList(){
+    
+    let todoListHTML = '';
+    for (let i = 0; i < todoList.length; i++) {
+       
+        const todoObject = todoList[i];
+        // const name = todoObject.name;
+        // const dueDate = todoObject.dueDate;
+        if(todoObject.name !=='' && todoObject.dueDate !==''){
+        const {name,dueDate} = todoObject;
+        const html = `<div>
+             ${name}</div>
+             <div> ${dueDate}</div>
+             <button
+              onclick="
+                todoList.splice(${i},1);
+                renderTodoList();
+             " class="delete-button">Delete</button> 
+            `;
+        todoListHTML+=html;
+        }  
+    }
+    // console.log(todoListHTML);
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+    }
+function addTodo(){
+   const inputElement =  document.querySelector('.js-name-input');
+   const name = inputElement.value;
+   const inputDateElement = document.querySelector('.js-due-date');
+   const dueDate = inputDateElement.value;
+   console.log(name);
+
+   todoList.push({
+    name:name,
+    dueDate:dueDate
+    });
+
+//    console.log(todoList);
+
+   inputElement.value = '';
+   inputDateElement.value = '';
+   renderTodoList();
+}
