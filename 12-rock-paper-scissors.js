f8d1ec7dc26964ce551e78451610834b40f9647b
Manish <manishdwivedi45165@gmail.com>
2023-07-24 16:29:45 +0530

    added eventlistner functions in rock-paper game

--- 12-rock-paper-scissors.js
+++ 12-rock-paper-scissors.js
@@ -13,7 +13,17 @@ const score =JSON.parse(localStorage.getItem('score'))||
 
 updateScoreElement();
 
-
+document.body.addEventListener('keydown',(event)=>{
+  if(event.key === 'r'){
+    playGame('rock');
+  }
+  else if(event.key === 'p'){
+    playGame('paper');
+  }
+  else if(event.key === 's'){
+    playGame('scissors');
+  }
+});
 
 function playGame(pickPlayer){
   const computerMove = pickComputerMove();
